02d3bfae9e1b283fd940aa0e8cbc43821a2c540c
j9nos <olah.janos.gergely@gmail.com>
2022-07-31 22:40:46 +0200

    Fixed boundaries

--- DoodleJump.java
+++ DoodleJump.java
@@ -257,11 +257,11 @@ class Player extends TwoDimensionalObject implements GameObject {
 	}
 
 	private void keepWithinBoundaries() {
-		if (this.getCoordinateX() < 0) {
-			this.setCoordinateX(500);
+		if (this.getCoordinateX() < 0 - GLOBAL_VARIABLES.PLAYER_WIDTH) {
+			this.setCoordinateX(GLOBAL_VARIABLES.WINDOW_WIDTH);
 		}
-		if (this.getCoordinateX() > 500) {
-			this.setCoordinateX(0);
+		if (this.getCoordinateX() > GLOBAL_VARIABLES.WINDOW_WIDTH) {
+			this.setCoordinateX(0 -GLOBAL_VARIABLES.PLAYER_WIDTH);
 		}
 	}
 
